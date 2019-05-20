d83ba3cc72616d0afdbb9d68ee53e1a8aaa3df06
Sadjad Fouladi <sfouladi@gmail.com>
2019-05-20 13:25:09 -0700

    tests/linked-list.cc: Create threads.

--- tests/linked-list.cc
+++ tests/linked-list.cc
@@ -1,3 +1,34 @@
+#include <memory>
+#include <thread>
+
 #include "rlu.hh"
 
-int main() { return 0; }
+using namespace std;
+
+const size_t NUM_THREADS = 10;
+
+int main( const int, char*[] )
+{
+  vector<thread> threads;
+  rlu::context::Global global_context;
+
+  for ( size_t i = 0; i < NUM_THREADS; i++ ) {
+    global_context.threads.emplace_back(
+        make_unique<rlu::context::Thread>( i, global_context ) );
+  }
+
+  for ( size_t i = 0; i < NUM_THREADS; i++ ) {
+    threads.emplace_back(
+        [&global_context]( const size_t thread_id ) {
+          auto& context = global_context.threads[thread_id];
+          context->thread_id();
+        },
+        i );
+  }
+
+  for ( size_t i = 0; i < NUM_THREADS; i++ ) {
+    threads[i].join();
+  }
+
+  return EXIT_SUCCESS;
+}
